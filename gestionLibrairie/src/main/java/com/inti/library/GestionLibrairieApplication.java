ce69bed0c6936de931bf3b7834326de3dc2b3843
llyodore <llyodore.mimoun@gmail.com>
2023-05-31 17:13:25 +0200

    Configuration de base SpringBoot

---
+++ gestionLibrairie/src/main/java/com/inti/library/GestionLibrairieApplication.java
@@ -0,0 +1,13 @@
+package com.inti.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionLibrairieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionLibrairieApplication.class, args);
+	}
+
+}
---
+++ gestionLibrairie/src/test/java/com/inti/library/GestionLibrairieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inti.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionLibrairieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
